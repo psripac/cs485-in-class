98267b8eef2293a8eb37c3e46d3bfa2d007179b8
psripac <psriprac@yahoo.com>
2019-10-21 16:54:32 -0700

    Added FPS behavior
    added feet, camera and FPS movement

--- In Class Work/Assets/PlayerController.cs
+++ In Class Work/Assets/PlayerController.cs
@@ -6,37 +6,58 @@ public class PlayerController : MonoBehaviour
 {
 
     public float speed;
-    public float gravity;
     public float jumpHeight;
 
-    public LayerMask Floor;
+    public LayerMask floor;
     public Transform feet;
 
     private Vector3 direction;
-    private Vector3 walkingVelocity;
-    private Vector3 fallingVelocity;
 
-    private CharacterController controller;
+    private Rigidbody rBody;
+
+    private float rotationSpeed = 1f;
+    private float minY = -60f;
+    private float maxY = 60f;
+    private float rotationY = 10f;
+    private float rotationX = 0f;
 
     // Start is called before the first frame update
     void Start()
     {
         speed = 5.0f;
-        gravity = 9.8f;
         jumpHeight = 2.5f;
-        direction = Vector3.zero;
-        fallingVelocity = Vector3.zero;
-        controller = GetComponent<CharacterController>();
+        rBody = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        direction = Vector3.zero;
         direction.x = Input.GetAxis("Horizontal");
         direction.z = Input.GetAxis("Vertical");
-
         direction = direction.normalized;
-        walkingVelocity = direction * speed;
+
+        if(direction.x != 0)
+        {
+            rBody.MovePosition(rBody.position + transform.right * direction.x * speed * Time.deltaTime);
+        }
+        if(direction.z != 0)
+        {
+            rBody.MovePosition(rBody.position + transform.forward * direction.z * speed * Time.deltaTime);
+        }
+
+        rotationX = transform.localEulerAngles.y + Input.GetAxis("Mouse X") * rotationSpeed;
+        rotationY += Input.GetAxis("Mouse Y") * rotationSpeed;
+        rotationY = Mathf.Clamp(rotationY, minY, maxY);
+
+        transform.localEulerAngles = new Vector3(-rotationY, rotationX, 0);
+
+        bool isGrounded = Physics.CheckSphere(feet.position, 0.1f, floor, QueryTriggerInteraction.Ignore);
+        if(Input.GetButtonDown("Jump") && isGrounded)
+        {
+            rBody.AddForce(Vector3.up * Mathf.Sqrt(jumpHeight * -2f * Physics.gravity.y), ForceMode.VelocityChange);
+        }
+
 
     }
 }
