10f05c2da3f2808d606cc6260b2ac69f4c1c8aaa
psripac <psriprac@yahoo.com>
2019-10-21 16:26:06 -0700

    Added Script
    Added PlayerController Script to player object

---
+++ In Class Work/Assets/PlayerController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float speed;
+    public float gravity;
+    public float jumpHeight;
+
+    public LayerMask Floor;
+    public Transform feet;
+
+    private Vector3 direction;
+    private Vector3 walkingVelocity;
+    private Vector3 fallingVelocity;
+
+    private CharacterController controller;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        speed = 5.0f;
+        gravity = 9.8f;
+        jumpHeight = 2.5f;
+        direction = Vector3.zero;
+        fallingVelocity = Vector3.zero;
+        controller = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        direction.x = Input.GetAxis("Horizontal");
+        direction.z = Input.GetAxis("Vertical");
+
+        direction = direction.normalized;
+        walkingVelocity = direction * speed;
+
+    }
+}
